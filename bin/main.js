b146144f7d0e823dc0516d486481fda46ba0fc11
J. Ryan Stinnett <jryans@gmail.com>
2012-10-28 11:02:23 -0500

    Table output

--- bin/main.js
+++ bin/main.js
@@ -1,7 +1,27 @@
+#!/usr/bin/env node
+
 var coreScore = require('../lib/main.js');
 var util = require('util');
+var argv = require('optimist').argv;
+var Table = require('cli-table');
+var _ = require('lodash');
 
-coreScore.scoreModules(function(err, data) {
+if (argv._.length === 0) {
+  console.error('Please specfiy one or more modules to analyze.');
+  process.exit(1);
+}
+
+var resultsTable = new Table({
+  head: ['Module', 'Usage']
+});
+
+var results = {};
+
+require('repl')._builtinLibs.forEach(function(coreMod) {
+  results[coreMod] = 0;
+});
+
+coreScore.scoreModules(argv._, function(err, data) {
   if (err) {
     util.error(err.stack);
     process.exit(1);
@@ -10,4 +30,20 @@ coreScore.scoreModules(function(err, data) {
   util.debug('done!')
 
   util.debug(util.inspect(data));
+
+  _.chain(data).values().forEach(function(moduleResults) {
+    _.forEach(moduleResults, function(used, coreMod) {
+      if (used) {
+        results[coreMod]++;
+      }
+    });
+  });
+
+  results = _.chain(results).pairs().sortBy(function(entry) {
+    return -entry[1];
+  }).value();
+
+  resultsTable.push.apply(resultsTable, results);
+
+  console.log(resultsTable.toString());
 });
